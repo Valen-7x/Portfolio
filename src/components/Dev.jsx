decb13b4f4d0a010bac6e06aec7615f203d4d57c
Valen-7x <valen.deorigen@gmail.com>
2023-09-21 14:04:29 -0300

    deploy + img

--- src/components/Dev.jsx
+++ src/components/Dev.jsx
@@ -5,7 +5,7 @@ export default function () {
     <div>
          <img
             className="rounded-full w-[11rem] h-[11.5rem] px-[1rem] lg:w-[14.5rem] lg:h-[15rem] transition-transform transform hover:scale-105"
-            src="public/valentin.jpg"
+            src="/valentin.jpg"
             alt=""
           />
     </div>

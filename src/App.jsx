d85dd9df8ab785a9d3f1cd47a42cf46da251ea98
Valen-7x <valen.deorigen@gmail.com>
2023-09-21 20:51:59 -0300

    valentin

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
           </div>
           <img
             className="rounded-full w-[11rem] h-[11.5rem] px-[1rem] lg:w-[14.5rem] lg:h-[15rem] transition-transform transform hover:scale-105"
-            src="/icons/valentin.jpg"
+            src="src\assets\icons\valentin.jpg"
             alt=""
           />
         </div>

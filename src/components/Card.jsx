060cf8b6eee585c5e07d61d14df83b936c410ce9
Valen-7x <valen.deorigen@gmail.com>
2023-09-25 10:32:26 -0300

    card styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,18 @@ import React from "react";
 export default function Card({ imageSrc }) {
   
   return (
-    <div
-      className=" hover:scale-25  mt-[1rem] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-300 w-[15rem] h-[10rem] md:w-[25rem] md:h-[18rem] rounded-2xl"
-     
-    >
-      <a href="https://app-clima-swart.vercel.app/" target="_blank" ><img src= {imageSrc}  alt="" className="flex  md:w-[25rem] md:h-[18rem] rounded-2xl" /></a>
-     
-      <h2 className="text-xl font-semibold"></h2>
+<div className="relative mt-[1rem] w-[15rem] h-[10rem] md:w-[25rem] md:h-[18rem] rounded-2xl overflow-hidden">
+  <a href="https://app-clima-swart.vercel.app/" target="_blank" className="block w-full h-full transition-transform transform hover:scale-105">
+    <img src={imageSrc} alt="" className="object-cover w-full h-full rounded-2xl" />
+    <div className="absolute  text-white inset-0 bg-gray-500 opacity-0 hover:opacity-90 ">
+      <div className="flex justify-center items-center h-[100%]">
+      <h2 className="text-white  font-bold  bottom-4  z-10">
+      App del Clima <br/> Obtene el clima de cualquier ciudad <br/> del mundo en tiempo real.
+      </h2>
+      </div>
     </div>
+  </a>
+</div>
+
   );
 }
